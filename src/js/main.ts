c57f17559e69c05727555eb08ecab2b41368e612
majora <samydahi2@gmail.com>
2025-12-28 17:37:31 +0100

    big progress

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,2 +1,30 @@
 import '../css/style.css'
 
+
+const englishLanguageLink = document.querySelector('.english-language')
+const frenchLanguageLink = document.querySelector('.french-language')
+
+function changeLanguage() {
+    if (document.documentElement.lang  === "en") {
+        englishLanguageLink?.classList.add('active-link')
+        frenchLanguageLink?.classList.remove('active-link')
+        
+    } else if (document.documentElement.lang  === "fr") {
+        frenchLanguageLink?.classList.add('active-link')
+        englishLanguageLink?.classList.remove('active-link')
+    }
+}
+
+changeLanguage()
+
+englishLanguageLink?.addEventListener('click', (e) => {
+    e.preventDefault()
+    document.documentElement.lang  = "en"
+    changeLanguage()
+})
+
+frenchLanguageLink?.addEventListener('click', (e) => {
+    e.preventDefault()
+    document.documentElement.lang  = "fr"
+    changeLanguage()
+})
