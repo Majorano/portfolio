39169559c27e2489a48bd780c62dad77117e1747
majora <samydahi2@gmail.com>
2025-12-28 18:42:35 +0100

    Add translation

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,12 +4,46 @@ import '../css/style.css'
 const englishLanguageLink = document.querySelector('.english-language')
 const frenchLanguageLink = document.querySelector('.french-language')
 
+
+
+async function translateTo(language: "en" | "fr") {
+
+    const attributesList = ["[data-text-i-am]", "[data-text-me]", "[data-text-language", "[data-text-webdev]", "[data-text-bitcoin]"]
+
+
+    let data: string
+    if (document.documentElement.lang !== "en" && language === "en") {
+        const r = await fetch('/public/translation/en.html')
+        if (!r.ok) return
+        data = await r.text()
+    } else if (document.documentElement.lang !== "fr" && language === "fr") {
+
+        const r = await fetch('/public/translation/fr.html')
+        if (!r.ok) return
+        data = await r.text()
+        
+    } else return
+    
+    const domParser = new DOMParser()
+    const newDom = domParser.parseFromString(data, 'text/html')
+    console.log(newDom)
+
+
+    for (const attribute of attributesList) {
+        const newText = newDom.querySelector<HTMLParagraphElement>(attribute)?.innerText
+        const element = document.querySelector<HTMLParagraphElement>(attribute)
+         if (element && newText) {
+            element.innerText = newText
+         }
+    }
+}
+
 function changeLanguage() {
-    if (document.documentElement.lang  === "en") {
+    if (document.documentElement.lang === "en") {
         englishLanguageLink?.classList.add('active-link')
         frenchLanguageLink?.classList.remove('active-link')
-        
-    } else if (document.documentElement.lang  === "fr") {
+
+    } else if (document.documentElement.lang === "fr") {
         frenchLanguageLink?.classList.add('active-link')
         englishLanguageLink?.classList.remove('active-link')
     }
@@ -19,12 +53,16 @@ changeLanguage()
 
 englishLanguageLink?.addEventListener('click', (e) => {
     e.preventDefault()
-    document.documentElement.lang  = "en"
+    translateTo("en")
+    document.documentElement.lang = "en"
     changeLanguage()
+
 })
 
 frenchLanguageLink?.addEventListener('click', (e) => {
     e.preventDefault()
-    document.documentElement.lang  = "fr"
+    translateTo('fr')
+    document.documentElement.lang = "fr"
     changeLanguage()
+
 })
