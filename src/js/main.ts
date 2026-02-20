cbcd229defdc1d81679c1c16cc6e36ba58c19dd7
majora <samydahi2@gmail.com>
2026-02-20 20:36:39 +0100

    add header and dark mode

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,10 +1,25 @@
 import '../css/style.css'
 
+// Handle dark mode
+
+// Adds dark class on html element
+document.documentElement.classList.toggle(
+    "dark",
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches),
+)
+// Dark mode button logic
+const darkModeButton = document.querySelector('[data-dark-mode-button]')
+darkModeButton?.addEventListener('click', () => {
+    document.documentElement.classList.toggle("dark")
+})
 
 const englishLanguageLink = document.querySelector('[data-english-language]')
 const frenchLanguageLink = document.querySelector('[data-french-language]')
 
 const translationFolder = "/translation"
+
+
 
 async function translateTo(language: "en" | "fr") {
 
@@ -21,34 +36,38 @@ async function translateTo(language: "en" | "fr") {
         const r = await fetch(`${translationFolder}/fr.html`)
         if (!r.ok) return
         data = await r.text()
-        
+
     } else return
-    
+
     const domParser = new DOMParser()
     const newDom = domParser.parseFromString(data, 'text/html')
-    console.log(newDom)
-
 
     for (const attribute of attributesList) {
         const newText = newDom.querySelector<HTMLParagraphElement>(attribute)?.innerText
         const element = document.querySelector<HTMLParagraphElement>(attribute)
-         if (element && newText) {
+        if (element && newText) {
             element.innerText = newText
-         }
+        }
     }
 }
 
 function changeLanguage() {
     if (document.documentElement.lang === "en") {
-        englishLanguageLink?.classList.add('text-paragraph')
-        frenchLanguageLink?.classList.remove('text-paragraph')
+        frenchLanguageLink?.classList.add('inactive-link')
+        englishLanguageLink?.classList.remove('inactive-link')
 
     } else if (document.documentElement.lang === "fr") {
-        frenchLanguageLink?.classList.add('text-paragraph')
-        englishLanguageLink?.classList.remove('text-paragraph')
+        englishLanguageLink?.classList.add('inactive-link')
+        frenchLanguageLink?.classList.remove('inactive-link')
     }
 }
 
+// boot the Frecnh language if it's the default language in the user's browser
+const navigatorLanguage = navigator.language.split('-')[0] as "en"|"fr"| string
+if (navigatorLanguage === "fr") {
+    translateTo('fr')
+    document.documentElement.lang = "fr"
+}
 changeLanguage()
 
 englishLanguageLink?.addEventListener('click', (e) => {
@@ -66,3 +85,5 @@ frenchLanguageLink?.addEventListener('click', (e) => {
     changeLanguage()
 
 })
+
+
