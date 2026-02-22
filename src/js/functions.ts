95670a02b11d39acacb0cf392859c6224ed41cdb
majora <samydahi2@gmail.com>
2026-02-22 12:45:32 +0100

    use json files for translation

--- src/js/functions.ts
+++ src/js/functions.ts
@@ -1,32 +1,28 @@
 import { englishLanguageLink, frenchLanguageLink, translationFolder } from "./main"
-
-
+/**
+ * This function needs:
+ * a translation folder, 
+ * .json translation files in which the keys are data-atrributes of real HTMLElemnts and the values are the text
+ */
 export async function translateTo(language: "en" | "fr") {
 
-    const attributesList = ["[data-text-me]", "[data-text-language", "[data-text-webdev]", "[data-text-bitcoin]"]
-
-
-    let data: string
+    let data: Object
     if (document.documentElement.lang !== "en" && language === "en") {
-        const r = await fetch(`${translationFolder}/en.html`)
+        const r = await fetch(`${translationFolder}/en.json`)
         if (!r.ok) return
-        data = await r.text()
+        data = await r.json()
     } else if (document.documentElement.lang !== "fr" && language === "fr") {
 
-        const r = await fetch(`${translationFolder}/fr.html`)
+        const r = await fetch(`${translationFolder}/fr.json`)
         if (!r.ok) return
-        data = await r.text()
+        data = await r.json()
 
     } else return
 
-    const domParser = new DOMParser()
-    const newDom = domParser.parseFromString(data, 'text/html')
-
-    for (const attribute of attributesList) {
-        const newText = newDom.querySelector<HTMLParagraphElement>(attribute)?.innerText
-        const element = document.querySelector<HTMLParagraphElement>(attribute)
-        if (element && newText) {
-            element.innerText = newText
+    for (const [key, value] of Object.entries(data)) {
+        const htmlElement = document.querySelector<HTMLElement>(`[${key}]`)
+        if (htmlElement) {
+            htmlElement.innerText = value
         }
     }
 }
